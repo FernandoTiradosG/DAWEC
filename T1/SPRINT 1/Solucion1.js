5a9b90d88fd808db1b645c463445ae9eb995d1c4
FernandoTiradosG <fernando.tiradosgonzalez.web@adaits.es>
2023-09-27 11:14:46 +0200

    Prueba spring1

--- T1/SPRINT 1/Solucion1.js
+++ T1/SPRINT 1/Solucion1.js
@@ -1,6 +1,14 @@
-let number = 5
-let string = "Hola, holita"
-let boolean = true
-let object = {}
-let null = null
-let undefined
+let numero = 5
+let texto = "Hola, holita"
+let booleano = true
+let objeto = {}
+let nulo = null
+let indefinido
+
+console.log(`${numero}, ${texto}, ${booleano}, ${objeto}, ${nulo}, ${indefinido}`)
+console.log(typeof(numero), typeof(texto), typeof(booleano), typeof(objeto), typeof(nulo), typeof(indefinido))
+
+let resultadoDiv = document.getElementById('resultado');
+resultadoDiv.innerHTML = `${numero}, ${texto}, ${booleano}, ${JSON.stringify(objeto)}, ${nulo}, ${indefinido}`;
+resultadoDiv.innerHTML += `<br>`;
+resultadoDiv.innerHTML += `${typeof(numero)}, ${typeof(texto)}, ${typeof(booleano)}, ${typeof(objeto)}, ${typeof(nulo)}, ${typeof(indefinido)}`;

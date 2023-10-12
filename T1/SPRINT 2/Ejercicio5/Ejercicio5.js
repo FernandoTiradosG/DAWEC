9985b0e59a7a3f7fcaca72b4bde5d3ce7f6b7a36
FernandoTiradosG <fernando.tiradosgonzalez.web@adaits.es>
2023-10-12 19:50:44 +0200

    Corrección

--- T1/SPRINT 2/Ejercicio5/Ejercicio5.js
+++ T1/SPRINT 2/Ejercicio5/Ejercicio5.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.addEventListener('click', function(event) {
-      var target = event.target;
-      var xpath = getXPath(target);
+      const target = event.target;
+      const xpath = getXPath(target);
       alert('XPath del elemento: ' + xpath);
   });
 
-  var iframe = document.getElementById('myIframe');
+  const iframe = document.getElementById('myIframe');
 
   // Escucha los mensajes enviados desde el iframe
   window.addEventListener('message', function(event) {
       if (event.source === iframe.contentWindow) {
-          var target = event.data.target;
-          var xpath = event.data.xpath;
+          const target = event.data.target;
+          const xpath = event.data.xpath;
           alert('XPath del elemento dentro del iframe: id(""myIframe")/' + xpath);
       }
   });
@@ -23,10 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
       if (element === document.body)
           return element.tagName;
 
-      var siblings = element.parentNode.childNodes;
-      var count = 0;
+      const siblings = element.parentNode.childNodes;
+      const count = 0;
       for (var i = 0; i < siblings.length; i++) {
-          var sibling = siblings[i];
+          const sibling = siblings[i];
           if (sibling === element)
               return getXPath(element.parentNode) + '/' + element.tagName + '[' + (count + 1) + ']';
           if (sibling.nodeType === 1 && sibling.tagName === element.tagName)
